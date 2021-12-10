fe87dc9bddd533b76b6892cdc717d9d6a8c4ee80
S Anand <root.node@gmail.com>
2021-12-10 13:14:40 +0530

    BLD: Release version 0.2.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 setup(
     name='gramexlayout',
-    version='0.2.0',
+    version='0.2.1',
     description='Layout algorithms for visualizations',
     long_description=(Path(__file__).parent / 'README.md').read_text(),
     long_description_content_type='text/markdown',
